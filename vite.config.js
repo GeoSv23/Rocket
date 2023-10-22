2341ac2b5d4f68795b54211451d95d73571195bb
GeoSv23 <115006657+GeoSv23@users.noreply.github.com>
2023-10-22 12:49:27 +0300

    добавлен config файл

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/rocket",
+  resolve: {
+    // alias: {
+    //   "@src": path.resolve(__dirname, "src"),
+    //   "@images": path.resolve(__dirname, "src/public/images"),
+    //   // Добавьте другие алиасы по мере необходимости
+    // },
+  },
+});
