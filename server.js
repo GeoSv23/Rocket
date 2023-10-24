20a0b0171ab65aae438c3b252d704684bd6ec3cc
GeoSv23 <115006657+GeoSv23@users.noreply.github.com>
2023-10-24 23:04:45 +0300

    первый коммит

--- server.js
+++ server.js
@@ -6,26 +6,23 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/send-email", (req, res) => {
-  const { input1, input2 } = req.body;
+  const { name, email, message } = req.body;
 
-  // Настройте транспорт для отправки электронных писем (в примере используется Gmail)
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
       user: "svidchenko23@gmail.com", // Ваш адрес электронной почты
-      pass: "svidchenkog.s.2305", // Ваш пароль от электронной почты
+      pass: "g.s.230596", // Пароль от электронной почты
     },
   });
 
-  // Настройте содержимое электронного письма
   const mailOptions = {
     from: "svidchenko23@gmail.com",
     to: "svidchenko23@gmail.com", // Адрес электронной почты получателя
     subject: "Новое сообщение от формы",
-    text: `Имя: ${input1}\nE-mail: ${input2}`,
+    text: `Имя: ${name}\nE-mail: ${email}\nСообщение: ${message}`,
   };
 
-  // Отправьте электронное письмо
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error(error);
@@ -37,6 +34,7 @@ app.post("/send-email", (req, res) => {
   });
 });
 
-app.listen(5173, () => {
-  console.log("Сервер запущен на порту 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
 });
